9bbce50cded51a70becfbfd0e2777550997a1c2c
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2026-04-30 03:34:39 +0300

    add Auto-Registration to DI

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Domain.Interfaces;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(Assembly.GetExecutingAssembly())
+            .AddClasses(classes => classes.AssignableTo<IScopedService>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo<ITransientService>())
+                .AsImplementedInterfaces()
+                .WithTransientLifetime()
+            .AddClasses(classes => classes.AssignableTo<ISingletonService>())
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime()
+        );
+
+        return services;
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+namespace Infrastructure;
+
+using Application.Interfaces.Queries;
+using Domain.Interfaces;
+using Infrastructure.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+public static class DependencyInjection
+{
+public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(ICommonQueries<>), typeof(CommonQueries<>));
+        services.AddScoped(typeof(ICommonCommands<>), typeof(CommonCommands<>));
+        services.Scan(scan => scan
+            .FromAssemblies(Assembly.GetExecutingAssembly())
+            .AddClasses(classes => classes.AssignableTo<IScopedService>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo<ITransientService>())
+                .AsImplementedInterfaces()
+                .WithTransientLifetime()
+            .AddClasses(classes => classes.AssignableTo<ISingletonService>())
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime()
+        );
+
+        return services;
+    }
+}
